8fd6efd9a6dc1d6c306546337ed90abccfae18ec
yzh119 <expye@outlook.com>
2020-10-13 01:20:16 +0800

    upd

--- toyrpc/comm_zmq.h
+++ toyrpc/comm_zmq.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "streambuffer.h"
 #include <atomic>
+#include <bits/stdint-uintn.h>
+#include <cstdlib>
 #include <exception>
 #include <mutex>
 #include <string>
@@ -25,8 +28,75 @@ class ZmqEP {
   uint16_t port_;
   uint64_t hash_;
 public:
-private:
+  ZmqEP() {}
+  ZmqEP(const std::string& ip, uint16_t port)
+    : ip_string_(ip),
+      port_(port) {
+    uint32_t ip_binary = 0;
+    size_t pos = 0;
+    for (int i = 0; i < 4; i++) {
+      uint8_t d = atoi(ip_string_.c_str() + pos);
+      ip_binary = ((ip_binary << 8) | d);
+      if (i < 3) {
+        pos = ip_string_.find('.', pos);
+        if (pos == ip_string_.npos) {
+          REMMY_ABORT("Error parsing ip address %s", ip_string_.c_str());
+        }
+        pos++;
+      }
+    }
+    hash_ = (((uint64_t)ip_binary << 16) | port_);
+  }
+
+  ZmqEP(uint64_t hash) {
+    hash_ = hash;
+    port_ = hash_ & 0xffff;
+  }
+
+  void SetPort(uint16_t port) {
+    port_ = port;
+    hash_ = ((hash_ >> 16) << 16) | port;
+  }
+
+  bool operator==(const ZmqEP& rhs) const {
+    return hash_ == rhs.hash_;
+  }
+
+  std::string ToString() const {
+    if (ip_string_.empty()) {
+      uint32_t binary = (uint32_t)(hash_ >> 16);
+      for (int i = 0; i < 4; i++) {
+        uint8_t d = (binary & 0xff000000) >> 24;
+        ip_string_ += std::to_string(d);
+        if (i != 3) ip_string_ += ".";
+        binary = (binary << 8);
+      }
+    }
+    return std::string("tcp://") + ip_string_ + ":" + std::to_string(port_);
+  }
+
+  uint64_t Hash() const {
+    return hash_;
+  }
+
+  uint16_t Port() const {
+    return port_;
+  }
+
+  void Serialize(StreamBuffer& buf) const {
+    buf.Write(&hash_, sizeof(hash_));
+  }
+
+  void Deserialize(StreamBuffer& buf) {
+    buf.Read(&hash_, sizeof(hash_));
+    port_ = hash_ & 0xffff;
+  }
 };
+
+template <>
+inline const std::string EPToString<ZmqEP>(const ZmqEP &ep) {
+  return ep.ToString();
+}
 
 };
 
--- toyrpc/concurrent_queue.h
+++ toyrpc/concurrent_queue.h
@@ -13,7 +13,62 @@ template <class T>
 class ConcurrentQueue {
 public:
 
+  explicit ConcurrentQueue(size_t max_elements = std::numeric_limits<size_t>::max())
+    : exit_now_(false),
+      max_elements_(max_elements) {}
+
+  bool Push(T&& e) {
+    return PushImpl(e);
+  }
+
+  bool Push(const T& e) {
+    return PushImpl(e);
+  }
+
+  bool Pop(T & rv) {
+    std::unique_lock<std::mutex> lk(mutex_);
+    while (queue_.empty() && !exit_now_)
+      cv_.wait(lk);
+    if (queue_.empty())
+      return false;
+    rv = queue_.front();
+    queue_.pop_front();
+    cv_.notify_one();
+    return true;
+  }
+
+  std::list<T> PopAll() {
+    std::lock_guard<std::mutex> lk(mutex_);
+    std::list<T> rv;
+    rv.swap(queue_);
+    return rv;
+  }
+
+  size_t Size() {
+    std::lock_guard<std::mutex> lk(mutex_);
+    return queue_.size();
+  }
+
+  void SignalForKill() {
+    std::lock_guard<std::mutex> lk(mutex_);
+    exit_now_ = true;
+    cv_.notify_all();
+  }
+
 private:
+
+  template<typename TT>
+  bool PushImpl(TT&& e) {
+    std::unique_lock<std::mutex> lk(mutex_);
+    while (queue_.size() >= max_elements_ && !exit_now_)
+      cv_.wait(lk);
+    if (queue_.size() >= max_elements_)
+      return false;
+    queue_.emplace_back(e);
+    cv_.notify_one();
+    return true;
+  }
+
   size_t max_elements_;
   std::list<T> queue_;
   std::mutex mutex_;
